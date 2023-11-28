b8c78bea5bb1fc7948095e1adc5b97d6d0290f0c
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-11-28 11:58:35 +0000

    投稿フォーム修正 seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Experience.find_or_create_by(id: 1) do |experience|
+  experience.name = '初心者'
+end
 
-Experience.create!(id: 1, name: '初心者')
-Experience.create!(id: 2, name: '経験者')
-Experience.create!(id: 3, name: '元整備士')
-Experience.create!(id: 4, name: '現役整備士')
-Experience.create!(id: 5, name: 'その他:業界関係者')
+Experience.find_or_create_by(id: 2) do |experience|
+  experience.name = '経験者'
+end
 
-Category.create!(id: 1, name: '雑談', body: '何でもOK 困ったら使う')
-Category.create!(id: 2, name: '挨拶', body: '挨拶する時に使う')
-Category.create!(id: 3, name: '質問', body: '誰かに質問したい時に使う')
-Category.create!(id: 4, name: '整備', body: '整備関係の投稿をするときに使う')
-Category.create!(id: 5, name: '自慢', body: 'とにかく自慢したいときに使う')
+Experience.find_or_create_by(id: 3) do |experience|
+  experience.name = '元整備士'
+end
 
-admin = Admin.new(
-  :id => 2,
-  :email => 'admin@admin',
-  :password => '000000',
-  )
-admin.save
+Experience.find_or_create_by(id: 4) do |experience|
+  experience.name = '現役整備士'
+end
+
+Experience.find_or_create_by(id: 5) do |experience|
+  experience.name = 'その他:業界関係者'
+end
+
+Category.find_or_create_by(id: 1) do |category|
+  category.name = '雑談'
+  category.body = '何でもOK 困ったら使う'
+end
+
+Category.find_or_create_by(id: 2) do |category|
+  category.name = '挨拶'
+  category.body = '挨拶する時に使う'
+end
+
+Category.find_or_create_by(id: 3) do |category|
+  category.name = '質問'
+  category.body = '誰かに質問したい時に使う'
+end
+
+Category.find_or_create_by(id: 4) do |category|
+  category.name = '整備'
+  category.body = '整備関係の投稿をするときに使う'
+end
+
+Category.find_or_create_by(id: 5) do |category|
+  category.name = '自慢'
+  category.body = 'とにかく自慢したいときに使う'
+end
+
+Admin.find_or_create_by(email: "admin@admin") do |admin|
+  admin.email = 'admin@admin'
+  admin.password = '000000'
+end
+
